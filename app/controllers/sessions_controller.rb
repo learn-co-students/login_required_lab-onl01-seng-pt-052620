20f27ff6f9bad27dc19cc286a50ce61684b9cf84
DmitriyMalayev <dmitriy.malayev@gmail.com>
2020-10-30 16:48:54 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,54 @@
+class SessionsController < ApplicationController 
+    def create
+        session[:name] = params[:name] 
+        if session[:name] == nil || session[:name] == "" 
+            redirect_to "/login_path"
+        else 
+            redirect_to "/"
+        end 
+    end
+    binding.pry 
+    def destroy
+        
+        if session[:name]
+            session.delete([:name])   
+        end
+    end
+end 
+
+
+
+
+
+
+# class SessionsController < ApplicationController 
+#     def new 
+#         if params[:name] == nil || params[:name] == ""
+#             redirect_to "/login_path"
+#         else 
+#             render "/"
+#         end 
+#     end 
+ 
+#     def create
+#         session[:name] = params[:name]
+#         if session[:name] == nil || session[:name] == ""
+#             redirect_to "/login"
+#         else 
+#             redirect_to "/"
+#         end  
+#     end
+
+#     def destroy
+#         reset_session 
+#         redirect_to logout_path   
+#     end
+
+# end  
+
+# # rails routes
+# # Prefix Verb URI Pattern       Controller#Action
+# #   root GET  /                 application#hello
+# #  login GET  /login(.:format)  sessions#new
+# #        POST /login(.:format)  sessions#create
+# # logout POST /logout(.:format) sessions#destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,30 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "application#current_user" 
+  resources :secrets, only: [:show]
+  resources :sessions, only: [:create, :destroy] 
 end
+
+
+
+
+
+# Rails.application.routes.draw do
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#   root 'application#hello'
+#   get '/login' => 'sessions#new'
+#   post '/login' => 'sessions#create'
+#   post '/logout' => 'sessions#destroy'
+# end
+
+
+# Rails.application.routes.draw do
+#   namespace :admin do 
+#     resources :preferences 
+#   end 
+#   resources :artists do
+#     resources :songs, only: [:index, :show]
+#   end
+#   resources :songs
+
+# end
